c2f0bf2b3a4ad7f2d01fddf1e66eaed248c4654b
Realmzer <120264700+Realmzer@users.noreply.github.com>
2024-03-30 12:26:20 -0400

    Update setup-windows.bat

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -11,7 +11,7 @@ haxelib install flixel-ui 2.5.0
 haxelib install flixel-tools 1.5.1
 haxelib install SScript 7.7.0
 haxelib install hxCodec
-haxelib install tjson 1.4.0
+haxelib install tjson
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
